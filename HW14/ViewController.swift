54be4868afafa967fc1826ef7da8cb661e22532d
Oleg Shashkov <olegs@citymed12.ru>
2019-08-15 11:52:17 +0300

    Initial Commit

---
+++ HW14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW14
+//
+//  Created by Oleg Shashkov on 15/08/2019.
+//  Copyright © 2019 citymed12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
